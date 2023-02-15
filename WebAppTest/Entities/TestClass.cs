61ef5a8a7b4ae69b0869176670c8734d68102706
Jakub Adamík <jakub.adamik72@gmail.com>
2023-02-15 13:58:01 +0100

    feat: add json serializer tests

---
+++ WebAppTest/Entities/TestClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WebAppTest.Entities;
+
+public class DummyClass
+{
+	public string? Id { get; set; }
+	public DummyEnum DummyEnum { get; set; }
+}
+
+
+
---
+++ WebAppTest/Unit/JsonSerializer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.Json;
+using Common;
+using WebApp.Entities;
+using WebAppTest.Entities;
+
+namespace WebAppTest.Unit;
+
+[TestClass]
+public class JsonSerializer
+{
+    [TestMethod]
+    public void GlobalOptionsSerialization()
+    {
+        // Arrange
+        var expected = "{\"id\":null,\"dummyEnum\":\"Two\"}";
+        var obj = new DummyClass{ DummyEnum = DummyEnum.Two };
+
+        // Act
+        var actual = System.Text.Json.JsonSerializer.Serialize(obj, Globals.JsonSerializerOptions);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void GlobalOptionsDeserialization()
+    {
+        // Arrange
+        var expected = new DummyClass { DummyEnum = DummyEnum.Two };
+        var json = "{\"dummyEnum\":\"Two\"}";
+
+        // Act
+        var actual = System.Text.Json.JsonSerializer.Deserialize<DummyClass>(json, Globals.JsonSerializerOptions);
+
+        // Assert
+        Assert.AreEqual(expected.Id, actual.Id);
+        Assert.AreEqual(expected.DummyEnum, actual.DummyEnum);
+    }
+}
+
